8d77d7e1663b2b9fa2ef4f2151feb97e57c25ed3
Pavankumar B <pavanbabunaik631@gmail.com>
2026-04-06 18:35:06 +0530

    connected supabase database

--- backend/schema_tracker/db.py
+++ backend/schema_tracker/db.py
@@ -5,6 +5,6 @@ def get_connection():
         host="db.xxdgfjmnjdhcxkihyuzk.supabase.co",
         database="schemasync",
         user="postgres",
-        password="pk876232kdm@HNR"
+        password="pk876232kdm@HNR",
         port="5432"
     )
