4fe5c867c304e7339c8e5de6f818932f52ad9e49
Pavankumar B <pavanbabunaik631@gmail.com>
2026-04-06 18:57:48 +0530

    use supabase pooler

--- backend/schema_tracker/db.py
+++ backend/schema_tracker/db.py
@@ -2,10 +2,5 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host="db.xxdgfjmnjdhcxkihyuzk.supabase.co",
-        database="schemasync",
-        user="postgres",
-        password="pk876232kdm@HNR",
-        port=5432,
-        sslmode="require"
+        "postgresql://postgres:[YOUR-PASSWORD]@db.xxdgfjmnjdhcxkihyuzk.supabase.co:5432/postgres"
     )
